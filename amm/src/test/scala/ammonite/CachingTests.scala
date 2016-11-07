c4d58f1637ca2498cc51cb67a98312c0cc60559a
Li Haoyi <haoyi.sg@gmail.com>
2016-11-07 22:27:08 +0800

    REPL compiles and runs, tests still red

--- amm/src/test/scala/ammonite/CachingTests.scala
+++ amm/src/test/scala/ammonite/CachingTests.scala
@@ -116,7 +116,7 @@ object CachingTests extends TestSuite{
       val interp = createTestInterp(storage)
       interp.interpApi.load.module(scriptPath/"TagBase.sc")
       interp.interpApi.load.module(scriptPath/"TagPrevCommand.sc")
-      interp.interpApi.load.ivy("com.lihaoyi" %% "scalatags" % "0.4.5")
+      interp.interpApi.load.ivy("com.lihaoyi" %% "scalatags" % "0.6.2")
       interp.interpApi.load.module(scriptPath/"TagBase.sc")
       val n = storage.compileCache.size
       assert(n == 5) // customLolz predef + two blocks for each loaded file
@@ -133,7 +133,7 @@ object CachingTests extends TestSuite{
         val x = 1337
         @
         val y = x
-        import $ivy.`com.lihaoyi::scalatags:0.5.4`, scalatags.Text.all._
+        import $ivy.`com.lihaoyi::scalatags:0.6.2`, scalatags.Text.all._
         """)
       val scriptFile = tmp("""div("<('.'<)", y).render""")
 
@@ -155,7 +155,7 @@ object CachingTests extends TestSuite{
       write(
         predefFile,
         """
-        import $ivy.`com.lihaoyi::scalatags:0.5.4`; import scalatags.Text.all._
+        import $ivy.`com.lihaoyi::scalatags:0.6.2`; import scalatags.Text.all._
         val y = 31337
         """
       )
